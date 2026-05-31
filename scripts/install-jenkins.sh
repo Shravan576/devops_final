313423d95e50b7b29e7d064cb7b4f92129146891
Shravan Sulgekar <shravansulgekar00@gmail.com>
2026-05-31 17:02:08 +0530

    fix: update jenkins installation GPG key to 2026 version

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -22,11 +22,12 @@ sudo rm -f /usr/share/keyrings/jenkins-keyring.asc
 sudo rm -f /usr/share/keyrings/jenkins-keyring.gpg
 sudo rm -f /etc/apt/sources.list.d/jenkins.list
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.gpg
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # 4. Add Jenkins Repository
 echo "4. Registering Jenkins Debian Repository..."
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # 5. Install Jenkins
 echo "5. Installing Jenkins automation server..."
